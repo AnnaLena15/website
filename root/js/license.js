d84da0b279128ee8a977a993112f0cb683b59f96
AnnaLena15 <94771563+AnnaLena15@users.noreply.github.com>
2022-01-01 19:41:26 +0100

    Add files via upload

---
+++ root/js/license.js
@@ -0,0 +1,9 @@
+const data = {
+    "text": "<div>Terms Of Service. <br> Ihr seidt Berechtigt diesen Bot zu nutzen Und nicht zu Missbrauchen<br> Ihr Fragt was Mit missbrauch gemeint ist Ok dan liest genau<br> Spam,Ausnutzung der Datenbank <br>Es gelten die Discord Terms Of Service <a href='https://discord.com/terms' >Terms Of Service</a> <br> Und Community Richtlinien <a href='https://discord.com/guidelines' >Community Richtlinien Discord</a></div>"
+}
+
+document.getElementById("text").innerHTML += data.text;
+
+function back(){
+    history.back();
+}
